cb4028d8ab51c7115b8376e2f2cd6cf43de1de9b
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-10-29 00:37:23 +0200

    general sync

--- install.cmd
+++ install.cmd
@@ -1,1 +1,3 @@
-pyinstaller -i favicon.ico --onefile Photo-Video-Album-Organizer.py
+set "currentDirectory=%cd%
+pyinstaller --distpath %currentDirectory% -i favicon.ico --onefile Photo-Video-Album-Organizer.py
+pause
